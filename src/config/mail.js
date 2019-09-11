527a234454a73704f88f26e133f16612fb9a5171
Cristhyan <cristhyan.ko@outlook.com>
2019-09-11 15:25:25 -0400

    new features - send mail, purchase and ad info

---
+++ src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+	host: "smtp.mailtrap.io",
+	port: "2525",
+	secure: false,
+	auth: {
+		user: "20020742d638a8",
+		pass: "786466d9fdb8b0"
+	}
+};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,24 @@
 const express = require("express");
-const routes = express.Router();
-
-const UserController = require("./app/controllers/UserController");
-const SessionController = require("./app/controllers/SessionController");
+const controllers = require("./app/controllers");
 const authMiddleware = require("./app/middlewares/auth");
 
-routes.post("/sessions", SessionController.store);
-routes.post("/users", UserController.store);
+const routes = express.Router();
 
-routes.get("/teste", authMiddleware, (req, res) => {
-	res.json({ ok: true });
-});
+routes.post("/sessions", controllers.SessionController.store);
+routes.post("/users", controllers.UserController.store);
+
+routes.use(authMiddleware);
+
+/** Ads */
+routes.get("/ads", controllers.AdController.index);
+routes.get("/ads/:id", controllers.AdController.show);
+routes.post("/ads", controllers.AdController.store);
+routes.put("/ads/:id", controllers.AdController.update);
+routes.delete("/ads/:id", controllers.AdController.destroy);
+/** --- */
+
+/** Purchase */
+routes.post("/purchase", controllers.PurchaseController.store);
+/** --- */
 
 module.exports = routes;
